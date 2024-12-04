8094493abe91829922322601ede857a66042cbfc
oka <250376s@ugs.kochi-tech.ac.jp>
2024-12-04 22:53:32 +0900

    Socket.ioを追加し、オーディオ再生機能を実装

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// app.js
 const express = require('express');
-const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 8080;
 
 // publicディレクトリを静的ファイルとして提供
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('playAudio', () => {
+        io.emit('playAudio');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
